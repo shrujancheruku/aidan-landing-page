95b595e6adab47d7333515bdebf18762999ab16e
Shrujan Cheruku <shrujancheruku@gmail.com>
2026-06-22 09:36:53 +0700

    fix: specify static site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://aidanbelizaire.com",
+  output: "static",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
